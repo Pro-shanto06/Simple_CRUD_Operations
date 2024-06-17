4024be16c6bf2cb713f602704e7954498173bca7
Pro-shanto06 <proshantosaha1999@gmail.com>
2024-06-18 00:10:36 +0600

    phone number bug fix

--- backend/models/Employee.js
+++ backend/models/Employee.js
@@ -4,7 +4,7 @@ const employeeSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    phoneNumber: { type: Number, required: true },
+    phoneNumber: { type: String, required: true },
     isBlocked: { type: Boolean, default: false }
 });
 
